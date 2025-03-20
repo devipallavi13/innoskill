b7a2a8f2977ee1612cecbe7d9f9f646664ab2921
devipallavi13 <mdevipallavi2004@gmail.com>
2025-03-20 17:25:09 +0530

    second innoskill commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -o errexit  
+
+# Create and activate a virtual environment
+python -m venv venv  
+source venv/bin/activate  # Use 'venv/Scripts/activate' for Windows
+
+# Install dependencies
+pip install --upgrade pip  
+pip install -r requirements.txt  
+
+# Run Django migrations
+python manage.py makemigrations  
+python manage.py migrate  
+
+# Collect static files (Render serves them separately)
+python manage.py collectstatic --noinput  
